ccc0b52ab6066b73eddf954b39a7ca1fdd6a670b
Isaac Malak <isaacmalak123@gmail.com>
2026-01-06 16:25:34 +0200

    feat: implement availability fetching logic in AppController and AppModule

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AppModule } from './app.module';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private appModule: AppModule,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('availability')
+  async getAvailability(
+    @Query('startDate') startDate: string,
+    @Query('endDate') endDate: string,
+  ) {
+    try {
+      const response = await this.appModule.fetchAvailability(
+        new Date(startDate),
+        endDate ? new Date(endDate) : undefined,
+      );
+      return response;
+    } catch (error) {
+      return {
+        status: 'error',
+        message: error.message,
+        response: error.response?.data || error,
+      };
+    }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -15,14 +15,16 @@ type Calendar = {
 @Injectable()
 export class AppModule {
   private ghlToken: string;
+  private calendarId: string;
   constructor(
     private configService: ConfigService,
     private httpService: HttpService,
   ) {
     this.ghlToken = this.configService.get('GHL_API_TOKEN') ?? '';
+    this.calendarId = this.configService.get('GHL_CALENDAR_ID') ?? '';
   }
 
-  async getAvailability(calendarId: string, startDate: Date, endDate?: Date) {
+  async fetchAvailability(startDate: Date, endDate?: Date) {
     const query = new URLSearchParams();
 
     query.set('startDate', startDate.getTime().toString());
@@ -38,14 +40,16 @@ export class AppModule {
     query.set('ignoreDateRange', 'true');
 
     const url = new URL(
-      `https://services.leadconnectorhq.com/calendars/${calendarId}/free-slots`,
+      `https://services.leadconnectorhq.com/calendars/${this.calendarId}/free-slots`,
     );
     url.search = query.toString();
 
     const response = await this.httpService.axiosRef.get(url.toString(), {
       headers: {
         Authorization: `Bearer ${this.ghlToken}`,
+        Version: '2021-04-15',
       },
     });
+    return response.data;
   }
 }

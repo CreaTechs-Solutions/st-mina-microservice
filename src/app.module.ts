16eabdcc2b6d4c598f13ff4be76cad2fb6772f9d
Isaac Malak <isaacmalak123@gmail.com>
2026-01-06 15:50:19 +0200

    feat: add NestJS axios and config modules, implement availability fetching logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,51 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
+type Calendar = {
+  id: string;
+  name: string;
+};
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+@Injectable()
+export class AppModule {
+  private ghlToken: string;
+  constructor(
+    private configService: ConfigService,
+    private httpService: HttpService,
+  ) {
+    this.ghlToken = this.configService.get('GHL_API_TOKEN') ?? '';
+  }
+
+  async getAvailability(calendarId: string, startDate: Date, endDate?: Date) {
+    const query = new URLSearchParams();
+
+    query.set('startDate', startDate.getTime().toString());
+    if (endDate) {
+      query.set('endDate', endDate.getTime().toString());
+    } else {
+      const sevenDaysLater = new Date(startDate);
+      sevenDaysLater.setDate(sevenDaysLater.getDate() + 7);
+      query.set('endDate', sevenDaysLater.getDate().toString());
+    }
+
+    query.set('timezone', 'America/Chicago');
+    query.set('ignoreDateRange', 'true');
+
+    const url = new URL(
+      `https://services.leadconnectorhq.com/calendars/${calendarId}/free-slots`,
+    );
+    url.search = query.toString();
+
+    const response = await this.httpService.axiosRef.get(url.toString(), {
+      headers: {
+        Authorization: `Bearer ${this.ghlToken}`,
+      },
+    });
+  }
+}
